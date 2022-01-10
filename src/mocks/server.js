6514c3d71a91940361746ff8a73c7a8a78bacdf2
Ian Hollander <ianholla@gmail.com>
2022-01-10 11:32:25 -0500

    Add solution

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,8 +1,17 @@
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+import { films, skywalkerResults } from "./data";
 
 const handlers = [
-  // TODO: write the handlers!
+  rest.get("https://swapi.dev/api/people", (req, res, ctx) => {
+    return res(ctx.json(skywalkerResults));
+  }),
+  rest.get("https://swapi.dev/api/films/:id", (req, res, ctx) => {
+    const { id } = req.params;
+    const filmIndex = parseInt(id) - 1;
+
+    return res(ctx.json(films[filmIndex]));
+  }),
 ];
 
 export const server = setupServer(...handlers);
